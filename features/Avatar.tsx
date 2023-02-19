863d1ce95a99bdc8529b7475e08c9c2cd02aa72a
koki-shino <shino.k@x-point-1.net>
2023-02-19 16:24:49 +0900

    fix: 🐞  schema生成の修正

--- features/Avatar.tsx
+++ features/Avatar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { Database } from "@/schema";
+import { Database } from "@/types/schema";
 import Link from "next/link";
 type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
 
